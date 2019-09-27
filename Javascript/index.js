eb0de07b52f5e2a733ef147ef431e0006ef31e6c
noway909 <54964279+noway909@users.noreply.github.com>
2019-09-26 21:42:59 -0400

    project inizalized

---
+++ Javascript/index.js
@@ -0,0 +1,9 @@
+document.getElementById('date').innerHTML = new Date().toDateString();
+
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then( (json) => {
+      console.log(json);
+      document.querySelector('#post').innerHTML = json.body
+  });
+
